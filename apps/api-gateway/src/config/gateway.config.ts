6861e23a1398072b6dea04cfb6dfc986a351286d
Ido Avni <idoavni99@gmail.com>
2025-01-18 18:07:19 +0200

    Add some initial stuff for gateway

---
+++ apps/api-gateway/src/config/gateway.config.ts
@@ -0,0 +1,9 @@
+import { registerAs } from '@nestjs/config';
+export const gatewayConfig = registerAs('gateway', () => ({
+  port: process.env.PORT || 3000,
+  serviceDiscovery: process.env.SERVICE_DISCOVERY
+    ? JSON.parse(process.env.SERVICE_DISCOVERY)
+    : {
+        'registration-service': 'http://localhost:3001',
+      },
+}));
--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -4,11 +4,40 @@
  */
 
 import { Logger } from '@nestjs/common';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import { baseBootstrap } from '@jobie/nestjs-core';
 import { AppModule } from './app/app.module';
+import { gatewayConfig } from './config/gateway.config';
+import { ConfigType } from '@nestjs/config';
 
 baseBootstrap(AppModule).then(async (app) => {
-  const port = process.env.PORT || 3000;
+  const { port, serviceDiscovery } = app.get<ConfigType<typeof gatewayConfig>>(
+    gatewayConfig.KEY
+  );
+  for (const [serviceName, serviceUrl] of Object.entries(serviceDiscovery)) {
+    const proxy = createProxyMiddleware({
+      target: serviceUrl,
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/${serviceName}`]: '',
+      },
+      on: {
+        proxyRes: (proxyRes, req, res) => {
+          // Forward cookie being set from outside
+          if (proxyRes.headers['set-cookie']) {
+            res.setHeader('set-cookie', proxyRes.headers['set-cookie']);
+          }
+
+          // Forward auth header if being set from outside
+          if (proxyRes.headers['authorization']) {
+            res.setHeader('authorization', proxyRes.headers['authorization']);
+          }
+        },
+      },
+    });
+    app.use(`/${serviceName}`, proxy);
+    Logger.log(`Routing requests to ${serviceName} at /${serviceName}`);
+  }
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 });
