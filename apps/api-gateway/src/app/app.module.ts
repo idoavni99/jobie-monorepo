5a61473756e54d53a8a7355b14403f725440ae66
idoavni99 <Idoavni99@gmail.com>
2025-01-19 20:19:05 +0200

    Add some initial stuff for gateway and install shared deps (#1)
    * Add some initial stuff for gateway
    
    * Use pnpm in ci
    
    * Use pnpm version 10
    
    * Update ci.yml
    
    * HASHED PNPM VERSION
    
    * no legacy peer
    
    * Update ci.yml
    
    * Make all cicd shit work, add basic service discovery with swagger
    
    * Make E2E setup compiled server

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BaseAppModule } from '@jobie/nestjs-core';
+import { ConfigModule } from '@nestjs/config';
+import { gatewayConfig } from '../config/gateway.config';
 
 @Module({
-  imports: [BaseAppModule],
+  imports: [
+    BaseAppModule,
+    ConfigModule.forRoot({ isGlobal: true, load: [gatewayConfig] }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -6,9 +6,17 @@
 import { Logger } from '@nestjs/common';
 import { baseBootstrap } from '@jobie/nestjs-core';
 import { AppModule } from './app/app.module';
+import { gatewayConfig } from './config/gateway.config';
+import { ConfigType } from '@nestjs/config';
+import { setupProxyToService } from './proxy/setupProxy';
 
 baseBootstrap(AppModule).then(async (app) => {
-  const port = process.env.PORT || 3000;
+  const { port, serviceDiscovery } = app.get<ConfigType<typeof gatewayConfig>>(
+    gatewayConfig.KEY
+  );
+  for (const service of Object.entries(serviceDiscovery)) {
+    await setupProxyToService(app, service);
+  }
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 });
