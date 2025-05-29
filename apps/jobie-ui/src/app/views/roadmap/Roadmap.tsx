6342629c537544e0c59383de2c26b4c08d4b0962
Ido Avni <idoavni99@gmail.com>
2025-05-29 16:18:46 +0300

    Finally fix typescripttt

--- apps/jobie-ui/src/app/views/roadmap/Roadmap.tsx
+++ apps/jobie-ui/src/app/views/roadmap/Roadmap.tsx
@@ -1,19 +1,16 @@
 import { TMilestone } from '@jobie/milestone/types';
 import { TRoadmap } from '@jobie/roadmap/types';
-import {
-  Box,
-  CircularProgress,
-  Stack,
-  Typography
-} from '@mui/material';
+import { Box, CircularProgress, Stack, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { milestoneMangementApi } from '../../../api/milestone-management.api';
 import { roadmapCalibrationApi } from '../../../api/roadmap-calibration.api';
 import { useDataFetch } from '../../../hooks/use-data-fetch';
+import { useIsMobile } from '../../hooks/use-is-mobile';
 import { MilestonesList } from './roadmapMilestones/MilestoneList';
 
 export const Roadmap = () => {
+  const isMobile = useIsMobile();
   const navigate = useNavigate();
   const {
     loading,
@@ -27,9 +24,9 @@ export const Roadmap = () => {
           .filter((m) => m.status === 'active' || m.status === 'completed')
           .map((m) => m._id);
 
-        const { data: fullMilestones } = await milestoneMangementApi.get<TMilestone[]>(
-          `/batch?ids=${filteredMilestonesIds.join(',')}`
-        );
+        const { data: fullMilestones } = await milestoneMangementApi.get<
+          TMilestone[]
+        >(`/batch?ids=${filteredMilestonesIds.join(',')}`);
 
         const milestoneMap = new Map(fullMilestones.map((m) => [m._id, m]));
 
@@ -37,7 +34,9 @@ export const Roadmap = () => {
           const enriched = milestoneMap.get(m._id);
           if (enriched) {
             const totalSteps = enriched.steps.length || 1;
-            const completedSteps = enriched.steps.filter((s) => s.completed).length;
+            const completedSteps = enriched.steps.filter(
+              (s) => s.completed
+            ).length;
             const progress = (completedSteps / totalSteps) * 100;
             return { ...m, progress };
           }
@@ -63,7 +62,7 @@ export const Roadmap = () => {
   }, [loading, milestones, navigate]);
 
   return (
-    <Box p={4} width="100%" sx={{ overflowX: 'hidden' }}>
+    <Box p={4} width={isMobile ? '100%' : '100vw'}>
       <Typography variant="h4" mb={4} fontWeight="bold" textAlign="center">
         Your Career Roadmap
       </Typography>
@@ -73,23 +72,7 @@ export const Roadmap = () => {
           <CircularProgress />
         </Stack>
       ) : (
-        <Box
-            sx={{
-              overflowX: 'auto',
-              width: '100%',
-              pb: 4,
-            }}
-          >
-            {milestones && <MilestonesList milestones={milestones} />}
-            <Box
-              sx={{
-                height: '2px',
-                background: 'linear-gradient(to right, #ffffff33, #ffffff55)',
-                zIndex: 1,
-                mt: 2,
-              }}
-            />
-          </Box>
+        milestones && <MilestonesList milestones={milestones} />
       )}
 
       <Stack alignItems="center" mt={2}>
--- apps/jobie-ui/src/app/views/roadmap/roadmapMilestones/MilestoneList.tsx
+++ apps/jobie-ui/src/app/views/roadmap/roadmapMilestones/MilestoneList.tsx
@@ -1,42 +1,50 @@
 import { RoadmapMilestone } from '@jobie/roadmap/types';
 import { Stack } from '@mui/material';
+import { Ref } from 'react';
+import { ListProps, Virtuoso } from 'react-virtuoso';
 import { useIsMobile } from '../../../hooks/use-is-mobile';
 import { MilestoneListItem } from './MilestoneListItem';
 
 type Properties = {
   milestones: (RoadmapMilestone & { progress: number })[];
 };
-export const MilestonesList = ({ milestones }: Properties) => {
+
+const List: React.FC<ListProps & { ref?: Ref<HTMLDivElement> }> = ({
+  style,
+  children,
+  ref,
+}) => {
   const isMobile = useIsMobile();
   return (
     <Stack
-      direction={isMobile ? 'column' : 'row'}
-      gap={3}
-      alignItems="center"
-      overflow="auto"
-      sx={{
-        scrollBehavior: 'smooth',
-        pb: 6,
-
-        ...(isMobile
-          ? {
-              height: '360px',
-            }
-          : {
-              '&::-webkit-scrollbar': {
-                height: 3,
-              },
-              '&::-webkit-scrollbar-thumb': {
-                backgroundColor: 'rgba(255,255,255,0.08)',
-                borderRadius: 10,
-              },
-              '&::-webkit-scrollbar-track': {
-                backgroundColor: 'transparent',
-              },
-            }),
+      style={{
+        ...style,
+        display: 'flex',
       }}
+      gap={3}
+      flexDirection={isMobile ? 'column' : 'row'}
+      alignItems="center"
+      ref={ref}
     >
-      {milestones.map((milestone) => (
+      {children}
+    </Stack>
+  );
+};
+
+export const MilestonesList = ({ milestones }: Properties) => {
+  const isMobile = useIsMobile();
+  return (
+    <Virtuoso
+      style={{
+        height: isMobile ? globalThis.outerHeight : '360px',
+        width: isMobile ? '100%' : globalThis.outerWidth,
+      }}
+      components={{ List }}
+      data={milestones}
+      totalCount={milestones.length}
+      fixedItemHeight={240}
+      horizontalDirection={!isMobile}
+      itemContent={(_, milestone) => (
         <MilestoneListItem
           key={milestone.milestoneName}
           _id={milestone._id}
@@ -45,7 +53,7 @@ export const MilestonesList = ({ milestones }: Properties) => {
           status={milestone.status}
           progress={milestone.progress}
         />
-      ))}
-    </Stack>
+      )}
+    />
   );
 };
